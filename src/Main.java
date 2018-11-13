59da873179a7401393806790c26fe9a27e1304bb
km.ferdous <km.ferdous@revesoft.com>
2018-11-13 21:40:51 +0600

    jolly jump problem uva#10038

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import uva.Problem10038;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Problem10038.start();
+    }
+}
---
+++ src/uva/Problem10038.java
@@ -0,0 +1,32 @@
+package uva;
+
+import java.util.Scanner;
+
+public class Problem10038 {
+
+    public static void start() {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int len = sc.nextInt();
+            int[] dataArr = new int[len];
+            int[] diffArr = new int[len];
+            int diff, sum = 0;
+
+            for (int i = 0; i < len; i++) {
+                dataArr[i] = sc.nextInt();
+                if (i > 0) {
+                    diff = Math.abs(dataArr[i] - dataArr[i - 1]);
+                    if (diff < len && diffArr[diff] != 1) {
+                        diffArr[diff] = 1;
+                        sum++;
+                    }
+                }
+            }
+
+            if (sum == len - 1)
+                System.out.println("Jolly");
+            else
+                System.out.println("Not jolly");
+        }
+    }
+}
